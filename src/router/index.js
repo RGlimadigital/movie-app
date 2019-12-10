0c4fa2648c05d5f39624808eb6550ecd2f301c43
RgFullDev <rgfulldev@gmail.com>
2019-12-10 21:08:45 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import LatestMovie from '@/components/LatestMovie'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    routes: [{
+        path: '/',
+        name: 'LatestMovie',
+        component: LatestMovie
+    }]
+})
